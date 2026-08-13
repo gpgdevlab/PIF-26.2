d1d55fc1abd9bba13f717ef3ffe78ea0c53cb219
Pimentel <gabriel.pimentel1110@gmail.com>
2026-08-12 21:39:26 -0300

    add return

--- 01/01.c
+++ 01/01.c
@@ -3,10 +3,5 @@
 int main() {
     
     int num = 90;
-    printf("%d é um inteiro", num); 
-    printf("\n\tBom dia! Shirley.");
-    printf("Você já tomou café? \n");
-    printf("\n\nA solução não existe!\nNão insista.");
-    printf("Duas\tlinhas\tde\tsaída\nou\tuma?");
-    printf("%s\n%s\n%s\n", "um", "dois", "três");
+    return 0;
 }
--- 01/02.c
+++ 01/02.c
@@ -4,4 +4,6 @@ int main() {
     
     float e = 2.71828;
     printf("%.3f é um ponto flutuante", e); 
+    return 0;
+
 }
